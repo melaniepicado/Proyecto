bef2c4fa774485014b40bedffe6c1d1a4ab23c50
lawrence2807 <lawrencect28@gmail.com>
2022-08-18 12:51:14 -0500

    Se agregó la clase categoría de vehículos, son los diferentes vehículos que se pueden alquilar con sus respectivas categorías de cliente

---
+++ Estructura de datos/ProyectoFinal/src/CategoriaVehiculos.java
@@ -0,0 +1,64 @@
+public class CategoriaVehiculos {
+    
+    public void Zafiro(){
+        /*Coches disponibles
+        Maserati
+        pasajeros 2
+        2022
+        CÁMARA DE VISIÓN 360 GRADOS
+
+        Porsche
+        pasajeros 2
+        2021
+        CONEXIÓN A INTERNET
+
+        Rolls Royce
+        pasajeros 4
+        2022
+        SISTEMA DE ENTRETENIMIENTO EN LOS ASIENTOS POSTERIORES*/
+    }
+    
+    public void Oro(){
+        /*Coches disponibles
+        Ford 
+        pasajeros 4
+        2020
+        CÁMARA DE VISIÓN 360 GRADOS
+
+        BMW
+        pasajeros 4
+        2019
+        FRENADA AUTOMÁTICA DE EMERGENCIA
+
+        chevrolet
+        pasajeros 2
+        2019
+        CÁMARA DE VISIÓN 360 GRADOS*/
+    }
+    
+    public void Plata(){
+        /*Coches disponibles
+        Honda
+        pasajeros 4
+        2018
+        FRENADA AUTOMÁTICA DE EMERGENCIA
+
+        Nissan
+        pasajeros 4
+        2019
+        FRENADA AUTOMÁTICA DE EMERGENCIA*/
+    }
+    public void Bronce(){
+        /*Coches disponibles
+        Toyota
+        pasajeros 4
+        2017
+        FRENADA AUTOMÁTICA DE EMERGENCIA
+
+        Volvo
+        pasajeros 4
+        2016
+        FRENADA AUTOMÁTICA DE EMERGENCIA*/
+    }
+    
+}
